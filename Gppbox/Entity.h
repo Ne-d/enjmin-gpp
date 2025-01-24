c4bfd81115ac9cc088e005d2e3099f59a9d4bbc7
Ned <67144261+Ne-d@users.noreply.github.com>
2025-01-24 10:08:53 +0100

    Add entity class placeholder

---
+++ Gppbox/Entity.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <SFML/Graphics/Sprite.hpp>
+
+class Entity
+{
+public:
+	Entity();
+
+	void update();
+	
+	// Base coordinates
+	int cx;
+	int cy;
+	float xr;
+	float yr;
+
+	// Resulting coordinates
+	float xx;
+	float yy;
+
+	// Movement
+	float dx;
+	float dy;
+
+	// Graphics
+	sf::Sprite sprite;
+};
